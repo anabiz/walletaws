db2114fb863b289160817cc2e5cb87f698caad73
anabiz <anabizconcept9@gmail.com>
2021-02-13 10:49:50 +0100

    initial commit

---
+++ src/store/pg.ts
@@ -0,0 +1,14 @@
+import createConnectionPool, { sql } from '@databases/pg';
+// import { DATABASE_URL } from "../setting"
+
+// N.B. you will need to replace this connection
+// string with the correct string for your database.
+const db = createConnectionPool(
+  process.env.DATABASE_URL
+);
+
+
+ db.dispose();
+///console.log(DATABASE_URL)
+//console.log(process.env.DATABASE_URL)
+export { db, sql };
